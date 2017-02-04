3e54e91b1e363557bf472db82a95cafd6068076e
ghhubin <hubinmailbox@qq.com>
2017-02-04 22:26:58 +0800

    Add files via upload

--- Coder.java
+++ Coder.java
@@ -12,16 +12,23 @@ import java.io.UnsupportedEncodingException;
 
 public class  Coder
 {
-
-	private JFrame jf;	
 	private MD5Util  md5u = new MD5Util();
+	private Des3 des3u = new Des3();
+	
+	private JFrame jf;	
 	private String coding_scheme = "gbk";
 	
-	private JButton but1,but2;
+	//tab1
+	private JButton but1,but2;   
 	private MJTextField tf1,tf2,tf3,tf4,tf5,tf6,tf7;
 	
+	//tab2
 	private JButton rot13but,vg_but1,vg_but2,t2but2;
 	private MJTextField t2tf1,t2tf2,t2tf3,t2tf4,t2tf5,t2tf6,t2tf7;
+	
+	//tab3
+	private JButton des_but1,des_but2,des_but3;
+	private MJTextField des_tf1,des_tf2,des_tf3,des_tf4;
 	
 	Coder()
 	{
@@ -40,13 +47,17 @@ public class  Coder
 		
 		JPanel jpl1 = new JPanel();	
 		JPanel jpl2 = new JPanel();
+		JPanel jpl3 = new JPanel();
 		jpl1.setLayout(new FlowLayout());
 		jpl2.setLayout(new FlowLayout());
+		jpl3.setLayout(new FlowLayout());
 		
 		tp.addTab("code", jpl1);
 		tp.setEnabledAt(0,true);
 		tp.addTab("cipher", jpl2);
 		tp.setEnabledAt(1,true);
+		tp.addTab("3DES", jpl3);
+		tp.setEnabledAt(2,true);
         
 		//Tab1
 		tf1 = new MJTextField(40);
@@ -96,10 +107,34 @@ public class  Coder
 		jpl2.add(vg_but2);
 		jpl2.add(t2but2);
 
+		
+		//Tab3  DES
+		des_tf1 = new MJTextField(40);
+		des_tf2 = new MJTextField(40);
+		des_tf3 = new MJTextField(40);
+		des_tf4 = new MJTextField(40);
+		des_but1 = new JButton("Encrypt");
+		des_but2 = new JButton("Decrypt");
+		des_but3 = new JButton("Clear");
+				
+		jpl3.add(new JLabel("    Plaintext   "));
+		jpl3.add(des_tf1);
+		jpl3.add(new JLabel(" Ciphertext   "));
+		jpl3.add(des_tf2);
+		jpl3.add(new JLabel("             Key   "));
+		jpl3.add(des_tf3);
+		jpl3.add(new JLabel("               Iv    "));
+		jpl3.add(des_tf4);
+		jpl3.add(des_but1);
+		jpl3.add(des_but2);
+		jpl3.add(des_but3);
+				
 		//加载一下窗体上事件。
 		tab1_Event();
 		
 		tab2_Event();
+		
+		tab3_Event();
 
 		//显示窗体;
 		jf.setVisible(true);
@@ -303,8 +338,77 @@ public class  Coder
 		});
 	}
 
+	
+	//Tab3 Event
+	private void tab3_Event()
+	{	
+			// des3 encrypt
+			des_but1.addActionListener(new ActionListener() 
+			{
+				public void actionPerformed(ActionEvent e)
+				{
+					String strKey = des_tf3.getText();
+					String strIv = des_tf4.getText();
+					byte[] data,key,iv;
+					
+					if (strKey.length() != 24) {
+						des_tf2.setText("---The length of the key must be 24!----");
+						return;
+					}
+					if (strIv.length() != 8) {
+						des_tf2.setText("---The length of the IV must be 8!----");
+						return;
+					}
+					
+					try {
+					    data = des_tf1.getText().getBytes(coding_scheme);
+					    key = strKey.getBytes(coding_scheme);
+					    iv = strIv.getBytes(coding_scheme);
+					    byte[] ciphertext = des3u.des3EncodeCBC(key, iv, data); 
+					    des_tf2.setText(Base64.getEncoder().encodeToString(ciphertext));
+					} catch (Exception e1) { return; }
+				}
+			});
+			
+			//// des3 decrypt
+			des_but2.addActionListener(new ActionListener()
+			{
+				public void actionPerformed(ActionEvent e)
+				{
+					String strKey = des_tf3.getText();
+					String strIv = des_tf4.getText();
+					byte[] data,key,iv;
+					if (strKey.length() != 24) {
+						des_tf1.setText("---The length of the key must be 24!----");
+						return;
+					}
+					if (strIv.length() != 8) {
+						des_tf1.setText("---The length of the IV must be 8!----");
+						return;
+					}
+					try {
+					    key = strKey.getBytes(coding_scheme);
+					    iv = strIv.getBytes(coding_scheme);
+					    data = Base64.getDecoder().decode(des_tf2.getText());
+					    byte[] plaintext = des3u.des3DecodeCBC(key, iv, data); 
+					    des_tf1.setText(new String(plaintext));
+					} catch (Exception e1) { return; }
+				}
+			});
+			
+			des_but3.addActionListener(new ActionListener()
+			{
+				public void actionPerformed(ActionEvent e)
+				{
+								
+					des_tf1.setText("");
+					des_tf2.setText("");
+					
+				}
+			});
+		}
 
-	public static void main(String[] args) 
+	public static void main(String[] args)  throws Exception 
 	{
 		new Coder();
 	}
